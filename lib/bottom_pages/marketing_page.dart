a2b6cb511d35e7c7669fdc25a80715b4cb338bfb
Suraj <Surajsps@icloud.com>
2021-01-20 18:59:17 +0530

    initial commit

--- lib/bottom_pages/marketing_page.dart
+++ lib/bottom_pages/marketing_page.dart
@@ -1,10 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:new1/bottom_pages/store_qr_code.dart';
 
+import 'delivery_charge.dart';
+import 'discount_coupon.dart';
 import 'marketing_designs_page.dart';
 
-class BottomMarketingPage extends StatelessWidget {
+class BottomMarketingPage extends StatefulWidget {
+  @override
+  _BottomMarketingPageState createState() => _BottomMarketingPageState();
+}
+
+class _BottomMarketingPageState extends State<BottomMarketingPage> {
+  bool isActive = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,30 +26,103 @@ class BottomMarketingPage extends StatelessWidget {
             ListTile(
               title: Text('Marketing Designs'),
               leading: Icon(Icons.design_services_outlined),
-              trailing: Icon(Icons.arrow_forward_ios_sharp),onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>MarketingDesigns()),);
-            },
+              trailing: Icon(Icons.arrow_forward_ios_sharp),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MarketingDesigns(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Discount Coupons'),
+              leading: Icon(Icons.account_balance_wallet_outlined),
+              trailing: Icon(Icons.arrow_forward_ios_sharp),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DiscountCoupon(),
+                  ),
+                );
+              },
             ),
             ListTile(
               title: Text('Store QR Code'),
               leading: Icon(Icons.qr_code),
-              trailing: Icon(Icons.arrow_forward_ios_sharp),onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>StoreQRCode(),),);
-            },
+              trailing: Icon(Icons.arrow_forward_ios_sharp),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => StoreQRCode(),
+                  ),
+                );
+              },
             ),
             ListTile(
               title: Text('Delivery Charges'),
               leading: Icon(Icons.delivery_dining),
-              trailing: Icon(Icons.arrow_forward_ios_sharp),onTap: (){
-              print('Delivery Charges');
-            },
+              trailing: Icon(Icons.arrow_forward_ios_sharp),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DeliveryCharges(),
+                  ),
+                );
+              },
             ),
             ListTile(
-              title: Text('WhatsApp Chat Support'),
-              leading: Icon(Icons.chat),onTap: (){
-              print('WhatsApp Chat Support');
-            },
-            ),
+              leading: Icon(Icons.message),
+              title: Column(
+                children: [
+                  SwitchListTile(
+                    title: Text(
+                      'WhatsApp Chat Support',
+                      style: TextStyle(),
+                    ),
+                    value: isActive,
+                    activeColor: Colors.blue.withOpacity(.5),
+                    inactiveTrackColor: Colors.grey,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isActive = value;
+                      });
+                      if (value == true) {
+                        final snackBar = SnackBar(
+                          content: Text(
+                            'WhatsApp Support is ON now!',
+                          ),
+                          duration: Duration(seconds: 2),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () {
+                              // Some code to undo the change.
+                            },
+                          ),
+                        );
+                        Scaffold.of(context).showSnackBar(snackBar);
+                      } else {
+                        final snackBar = SnackBar(
+                          content: Text('WhatsApp Support is OFF now!'),
+                          duration: Duration(seconds: 2),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () {
+                              // Some code to undo the change.
+                            },
+                          ),
+                        );
+                        Scaffold.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/bottom_pages/store_qr_code.dart
+++ lib/bottom_pages/store_qr_code.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+import 'package:flutter/rendering.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StoreQRCode extends StatelessWidget {
@@ -5,36 +9,195 @@ class StoreQRCode extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Get your QR Code'),
-        ),
-        body: Card(
-          margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
-          child: Container(
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(decoration: BoxDecoration(color: Colors.blueAccent,borderRadius: BorderRadius.horizontal()),
-                    height: MediaQuery.of(context).size.width,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    height: MediaQuery.of(context).size.width,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                ),
-              ],
-            ),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
+          appBar: AppBar(
+            title: Text('Get your QR Code'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  margin:
+                      EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    topRight: Radius.circular(20),
+                                  ),
+                                ),
+                                height: MediaQuery.of(context).size.width,
+                                width: MediaQuery.of(context).size.width,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                  ),
+                                ),
+                                height: MediaQuery.of(context).size.width,
+                                width: MediaQuery.of(context).size.width,
+                              ),
+                            ),
+                          ],
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.width,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'We support',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                  Container(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '#Digitalindia ',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                            height: 30,
+                                            width: 30,
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                            ),
+                                            child: Icon(Icons.flag))
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.symmetric(vertical: 30.5),
+                                    child: Text(
+                                      'Scan QR to check our products',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              flex: 1,
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Card(
+                                    child: QrImage(
+                                      data: "Vffxgd",
+                                      version: QrVersions.auto,
+                                      size: 120.0,
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              flex: 1,
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Vffxgd',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    'mydukaan.io/vffxgd',
+                                    style: TextStyle(color: Colors.blueAccent),
+                                  ),
+                                  Expanded(
+                                    child: Container(),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 5),
+                                        child: Container(
+                                          child: Icon(
+                                            Icons.shopping_bag_outlined,
+                                            color: Colors.white,
+                                          ),
+                                          height: 35,
+                                          width: 35,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            color: Colors.blueAccent,
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                        'dukaan',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              flex: 1,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )),
+              CupertinoButton(
+                child: Row(mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Icon(Icons.picture_as_pdf_outlined),
+                    ),
+                    Text('Print PDF')
+                  ],
+                ),
+                onPressed: () {
+                  print('Pdf');
+                },
+                color: Colors.blueAccent,
+              )
+            ],
+          )),
     );
   }
 }
